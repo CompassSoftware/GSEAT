052d72c720e708767ef30584db75773bc6cc7fa0
Thomas Neumayr <tom.neumayr@gmail.com>
2019-11-03 22:28:03 -0500

    added basic files (makefile, style check, extraction class and extraction test)

---
+++ Extraction/Extraction.java
@@ -0,0 +1,16 @@
+/**
+ * Extraction class.
+ * @author Thomas Neumayr
+ * @version 1.0
+ */
+public class Extraction
+{
+    /**
+    * Function to print the string "Hello, world!" to the console screen.
+    * @param args Unused arguments.
+    */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, world!");
+    }
+}
