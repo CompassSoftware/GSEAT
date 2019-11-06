f1af7c0208ee156aacadbd037c2e8a1e98b84242
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-11-06 09:04:16 -0500

    Initial code for extraction from github api

--- Extraction/Extraction.java
+++ Extraction/Extraction.java
@@ -1,16 +1,34 @@
 /**
  * Extraction class.
  * @author Thomas Neumayr
+ * @author Jacob MacFarland
  * @version 1.0
  */
-public class Extraction
-{
-    /**
-    * Function to print the string "Hello, world!" to the console screen.
-    * @param args Unused arguments.
-    */
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello, world!");
+
+
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.RepositoryId;
+
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.eclipse.egit.github.core.Repository;
+
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import java.io.*;
+
+public class Extraction {
+    public static void main(String[] args) {
+        GitHubClient client = new GitHubClient();
+        client.setCredentials("jacobmacfarland", "");
+
+        RepositoryService service = new RepositoryService();
+        service.getClient().setCredentials("jacobmacfarland", "");
+        try {
+            for (Repository repo : service.getRepositories("defunkt"))
+                System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
