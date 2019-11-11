35971229c12ea9b4a7bf46d2b1a39abb3cd625ee
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-11-11 16:21:40 -0500

    Refactored extraction class

--- Extraction/Extraction.java
+++ Extraction/Extraction.java
@@ -35,53 +35,52 @@ public class Extraction
     */
     public static void main(String[] args) 
     {
-        String repoUrl = new String("https://api.github.com/repos/jacobmacfarland/FinanceCalc");
+      
+      Extraction extractor = new Extraction();
+    
+      JSONObject jsonObject = extractor.getJsonFromUrl("https://api.github.com/repos/jacobmacfarland/FinanceCalc");
+      String repoName = jsonObject.getString("description");
+      String repoCollaboratorsURL = jsonObject.getString("collaborators_url");
+      String repoCommitsURL = jsonObject.getString("commits_url");
+      String repoCommentsURL = jsonObject.getString("comments_url");
+      String repoIssuesURL = jsonObject.getString("issues_url");
+      String repoIssueCommentsURL = jsonObject.getString("issue_comment_url");
 
-        try {
-            URL url = new URL(repoUrl);
-            URLConnection urlConnection = url.openConnection();
-            HttpURLConnection connection = null;
-
-            if (urlConnection instanceof HttpURLConnection) {
-                connection = (HttpURLConnection) urlConnection;
-            }
-            else {
-                  System.out.println("Please input the url address: ");
-                  return;
-             }
-             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-             String current;
-             String urlString = "";
-             while((current = in.readLine()) != null) {
-                 urlString += current;
-             }
-
-             /** Handle file with json type and seach for the info with name and html_url
-              * @param repo_name        the name of user's each repo
-              * @param url_name         the url of user's each repo
-              */
-             //System.out.println(urlString);
-             JSONObject jsonObject = JSONObject.fromObject(urlString);
-             String repoName = jsonObject.getString("description");
-             String repoCollaboratorsURL = jsonObject.getString("collaborators_url");
-             String repoCommitsURL = jsonObject.getString("commits_url");
-             String repoCommentsURL = jsonObject.getString("comments_url");
-             String repoIssuesURL = jsonObject.getString("issues_url");
-             String repoIssueCommentsURL = jsonObject.getString("issue_comment_url");
-
-             System.out.println("\n\nRepository name: " + repoName);
-             System.out.println("\n\nRepository collaborators URL: " + repoCollaboratorsURL);
-             System.out.println("\n\nRepository commits URL: " + repoCommitsURL);
-             System.out.println("\n\nRepository comments URL: " + repoCommentsURL);
-             System.out.println("\n\nRepository issues URL: " + repoIssuesURL);
-             System.out.println("\n\nRepository issue comments URL: " + repoIssueCommentsURL);
+      System.out.println("\n\nRepository name: " + repoName);
+      System.out.println("\n\nRepository collaborators URL: " + repoCollaboratorsURL);
+      System.out.println("\n\nRepository commits URL: " + repoCommitsURL);
+      System.out.println("\n\nRepository comments URL: " + repoCommentsURL);
+      System.out.println("\n\nRepository issues URL: " + repoIssuesURL);
+      System.out.println("\n\nRepository issue comments URL: " + repoIssueCommentsURL);
              
-             //JSONObject owner = jsonObject.getJSONObject("owner");
-
+    }
+    
+    public JSONObject getJsonFromUrl(String repoUrl) {
+      JSONObject jsonObject = null;
+      try {
+        URL url = new URL(repoUrl);
+        URLConnection urlConnection = url.openConnection();
+        HttpURLConnection connection = null;
+        
+        if (urlConnection instanceof HttpURLConnection) {
+            connection = (HttpURLConnection) urlConnection;
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        else {
+          System.out.println("Please input the url address: ");
+          return null;
         }
-
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String current;
+        String urlString = "";
+        while((current = in.readLine()) != null) {
+            urlString += current;
+        }
+        jsonObject = JSONObject.fromObject(urlString);
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+      return jsonObject;
+      
     }
 }
