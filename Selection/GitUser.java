6fe7da66a69cc12492f82c430913e0b30ff0899a
yuanboz <yuanboz@student2.cs.appstate.edu>
2019-11-18 22:01:24 -0500

    compelete case 1 and case 2

--- Selection/GitUser.java
+++ Selection/GitUser.java
@@ -20,6 +20,8 @@ public class GitUser {
     static String Url;
     static String repo_name;
     static String repo_url;
+    static String org_name;
+    static String url_name;
     static Scanner sc = new Scanner(System.in);
     static String input;
     static String username;
@@ -104,12 +106,11 @@ public class GitUser {
                     for (int i = 0; i < list_repo_name.size(); i++) {
                         System.out.println(i+1+ "." +  "Repository: " + list_repo_name.get(i) + "    " + list_repo_url.get(i));
                     }
-                    break;
                 case 2:
                     System.out.println("Please input TOKEN: ");
                     TOKEN = sc.nextLine();
-                    JSONArray jsonObject1 = null;
-                    Url = ("https://api.github.com/orgs/JaysGitLab/members");
+                    JSONObject jsonObject1 = null;
+                    Url = ("https://api.github.com/orgs/JaysGitLab");
                     try {
                         URL url = new URL(Url);
                         URLConnection urlConnection = url.openConnection();
@@ -129,23 +130,14 @@ public class GitUser {
                         while ((current = in.readLine()) != null) {
                             urlString += current;
                         }
-                        System.out.println(urlString);
-                        jsonObject1 = JSONArray.fromObject(urlString);
+                        //System.out.println(urlString);
+                        jsonObject1 = JSONObject.fromObject(urlString);
+                        org_name = jsonObject1.getString("login");
+                        url_name = jsonObject1.getString("url");
+                        System.out.println("Organization: " + org_name + "  " + url_name);
                     }
                     catch (IOException e) {
                         e.printStackTrace();
-                    }
-                    JSONArray jsonarray_2 = JSONArray.fromObject(jsonObject1);
-                    if (jsonarray_2.size() > 0) {
-                        for (int i = 0; i < jsonarray_2.size(); i++) {
-                            JSONObject jsonObject = JSONObject.fromObject(jsonarray_2.get(i).toString());
-                            repo_name = jsonObject.getString("status");
-                            JSONObject owner = jsonObject.getJSONObject("owner");
-                            repo_url = jsonObject.getString("url");
-                            list_repo_name.add(repo_name);
-                            list_repo_url.add(repo_url);
-                        }
-                        System.out.println(repo_name);
                     }
                     break;
         }
