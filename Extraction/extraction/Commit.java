c6ecd0e7a4c85382ced3e364c055a63241a3c9c6
Thomas Neumayr <tom.neumayr@gmail.com>
2019-11-21 23:55:40 -0500

    style fixes

--- Extraction/extraction/Commit.java
+++ Extraction/extraction/Commit.java
@@ -1,61 +1,90 @@
 package extraction;
 import java.util.Date;
 
-public class Commit
-{
+ /**
+ * Commit class.
+ * @author Nischinth Murari
+ * @version 1.0
+ */
+public class Commit {
     String info;
     String userName;
     Date dateCreated;
     Date dateUpdated;
 
-    /*
-     * Constructor that take a Collaborator object and a String for info about the inital commit. 
-     */
-    public Commit(String info, Collaborator C)
-    {
-        this.userName = C.getUserName();
+    /**
+    * Constructor for commit object.
+    * @param info - Info text of the commit.
+    * @param c - Collaborator/Creator of the commit.
+    */
+    public Commit(String info, Collaborator c) {
+        this.userName = c.getUserName();
         this.info = info;
         this.dateCreated = null;
         this.dateUpdated = null;
     }
 
-    public void setInfo(String info)
-    {
+    /**
+     * Setter for info.
+     * @param info - Info text of the commit.
+     */
+    public void setInfo(String info) {
         this.info = info;
     }
 
-    public void setUserName(String userName)
-    {
+    /**
+     * Setter for userName.
+     * @param userName - User name of the commit creator.
+     */
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public void setdateCreated(Date d)
-    {
-        this.dateCreated = d;
+    /**
+     * Setter for dateCreated.
+     * @param dateCreated - Date of the creation of the commit.
+     */
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
     }
 
-    public void setDateUpdated(Date d)
-    {
-        this.dateUpdated = d;
+    /**
+     * Setter for dateUpdated.
+     * @param dateUpdated - Date of the update of the commit.
+     */
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
     }
 
-    public String getInfo()
-    {
+    /**
+     * Getter for info.
+     * @return Info text of the commit.
+     */
+    public String getInfo() {
         return this.info;
     }
 
-    public String getUserName()
-    {
+    /**
+     * Getter for userName.
+     * @return User name of the commit creator.
+     */
+    public String getUserName() {
         return this.userName;
     }
 
-    public Date getDateCreated()
-    {
+    /**
+     * Getter for dateCreated.
+     * @return Date of the creation of the commit.
+     */
+    public Date getDateCreated() {
         return this.dateCreated;
     }
 
-    public Date getDateUpdated()
-    {
+    /**
+     * Getter for dateUpdated.
+     * @return Date of the update of the commit.
+     */
+    public Date getDateUpdated() {
         return this.dateUpdated;
     }
 }
