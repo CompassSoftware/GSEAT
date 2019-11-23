d99909d8ff01929fd51c7e4eabbb0ea5d4398608
Yuanboz <yuanboz@appstate.edu>
2019-11-22 20:44:13 -0500

    update code

---
+++ Selection/selection/Demo.java
@@ -0,0 +1,18 @@
+/* Demo.java
+ *
+ * Selection team
+ *
+ * @author Yuanbo Zhou
+ *
+ * @version: 1.0
+ *
+ */
+
+import selection.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        Greeting g =new Greeting();
+        g.greeting();
+    }
+}
