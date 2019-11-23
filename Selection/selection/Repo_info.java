50562b72712097c4d4d3d7c32cdaa66103ca48c1
Yuanboz <yuanboz@appstate.edu>
2019-11-22 21:27:33 -0500

    update

---
+++ Selection/selection/Repo_info.java
@@ -0,0 +1,30 @@
+/** Repo_info.java
+ *
+ *  API connection to get Json file. Fetch name and url of repository.
+ *
+ * @author Zack
+ *
+ * @version 1.0
+ */
+
+package selection;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import net.sf.json.*;
+
+import static selection.Greeting.*;
+
+public class Repo_info {
+
+    static String Url;
+    static String repo_name;
+    static String repo_url;
+    static String TOKEN;
+    static String org_name;
+    static String url_name;
+    static String selection;
+
+  
+}
